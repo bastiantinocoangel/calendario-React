93c2d97bffb5cef42c2ced508fc1a9dc9c8a3468
bastiantinocoangel <47192223+bastiantinocoangel@users.noreply.github.com>
2021-03-02 19:58:10 -0600

    Add files via upload

---
+++ precache-manifest.52410964ae624ada54f9a558dd23fa55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29951e57c6ab7ce57254ac05b2d2850",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "22985e9b71fa33886057",
+    "url": "/react/static/css/main.c6c3791b.chunk.css"
+  },
+  {
+    "revision": "92adfd0cfc0e924807e2",
+    "url": "/react/static/js/2.f38ce821.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react/static/js/2.f38ce821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22985e9b71fa33886057",
+    "url": "/react/static/js/main.272e0602.chunk.js"
+  },
+  {
+    "revision": "fd7296bed65d26a8b278",
+    "url": "/react/static/js/runtime-main.bb2b820c.js"
+  }
+]);
